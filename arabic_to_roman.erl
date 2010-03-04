78bdab4c998d7995b3ed1d676fa86aa82c781532
Adam Walters <ajwalters@gmail.com>
2010-03-04 17:45:15 -0600

    Added support for Arabic->Roman for roman numerals built using addition from left->right (III, VII, etc)

--- arabic_to_roman.erl
+++ arabic_to_roman.erl
@@ -3,12 +3,50 @@
 -compile(export_all).
 
 convert(ArabicNumber) ->
-  case ArabicNumber of
+  case reserved_numeral(ArabicNumber) of
+    nomatch ->
+      build_by_addition(ArabicNumber);
+    Reserved ->
+      Reserved
+  end.
+
+build_by_addition(Numeral) ->
+  {RomanNumeralBase, Remainder} = base_for_addition(Numeral),
+  build_by_addition(Remainder, RomanNumeralBase).
+
+build_by_addition(0, RomanBuilder) ->
+  RomanBuilder;
+
+build_by_addition(Numeral, RomanBuilder) ->
+  build_by_addition(Numeral - 1, string:concat(RomanBuilder, "I")).
+
+base_for_addition(Numeral) ->
+  if
+    Numeral > 1000 ->
+      {reserved_numeral(1000), Numeral - 1000};
+    Numeral > 500 ->
+      {reserved_numeral(500), Numeral - 500};
+    Numeral > 100 ->
+      {reserved_numeral(100), Numeral - 100};
+    Numeral > 50 ->
+      {reserved_numeral(50), Numeral - 50};
+    Numeral > 10 ->
+      {reserved_numeral(10), Numeral - 10};
+    Numeral > 5 ->
+      {reserved_numeral(5), Numeral - 5};
+    true ->
+      {reserved_numeral(1), Numeral - 1}
+  end.
+
+reserved_numeral(Numeral) ->
+  case Numeral of
     1 -> "I";
     5 -> "V";
     10 -> "X";
     50 -> "L";
     100 -> "C";
     500 -> "D";
-    1000 -> "M"
+    1000 -> "M";
+    _ -> nomatch
   end.
+
--- test_arabic_to_roman.erl
+++ test_arabic_to_roman.erl
@@ -10,3 +10,13 @@ seven_key_symbols_test_() ->
     ?_assertEqual("C", arabic_to_roman:convert(100)),
     ?_assertEqual("D", arabic_to_roman:convert(500)),
     ?_assertEqual("M", arabic_to_roman:convert(1000))].
+
+% This tests that roman numerals that are built from left
+% to right adding the characters can be built
+% eg- III, VI, DIII, etc
+convert_roman_numerals_based_on_addition_test_() ->
+  [ ?_assertEqual("II", arabic_to_roman:convert(2)),
+    ?_assertEqual("III", arabic_to_roman:convert(3)),
+    ?_assertEqual("VI", arabic_to_roman:convert(6)),
+    ?_assertEqual("VII", arabic_to_roman:convert(7)),
+    ?_assertEqual("DIII", arabic_to_roman:convert(503))].
